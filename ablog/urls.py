c2a618b4a661aa74f28560891a7a1a02d04ab2f4
Preetam Raj <iitianpreetam@gmail.com>
2021-03-13 13:35:33 +0530

    settings updated

--- ablog/urls.py
+++ ablog/urls.py
@@ -1,4 +1,5 @@
-
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
@@ -8,3 +9,8 @@ urlpatterns = [
     # path('members/', include('django.contrib.auth.urls')),
     path('members/', include('members.urls'))
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
